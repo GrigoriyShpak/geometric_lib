d8a77e828275babbbf978179e5e083f113c3d512
Григорий <468117@niuitmo.ru>
2024-11-21 21:20:04 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,12 +1,10 @@
 def area(a, b, c):
     if a < 0 or b < 0 or c < 0:
-        print("incorrect input")
-        exit(-1)
+        raise ValueError("Incorrect input: side lengths cannot be negative")
     return (a + b + c) / 2
 
 
 def perimeter(a, b, c):
     if a < 0 or b < 0 or c < 0:
-        print("incorrect input")
-        exit(-1)
+        raise ValueError("Incorrect input: side lengths cannot be negative")
     return a + b + c
