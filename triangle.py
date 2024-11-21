833362a7ad3404f6530f6ae3100212f62bbdbd1b
Григорий <468117@niuitmo.ru>
2024-11-21 21:11:27 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,6 +1,12 @@
 def area(a, b, c):
+    if a < 0 or b < 0 or c < 0:
+        print("incorrect input")
+        exit(-1)
     return (a + b + c) / 2
 
 
 def perimeter(a, b, c):
+    if a < 0 or b < 0 or c < 0:
+        print("incorrect input")
+        exit(-1)
     return a + b + c
