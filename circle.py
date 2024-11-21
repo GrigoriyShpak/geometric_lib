f8859125dab840ef5d3f491e84c69646fbc927b5
Григорий <468117@niuitmo.ru>
2024-11-21 21:16:17 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -3,13 +3,11 @@ import math
 
 def area(r):
     if r < 0:
-        print("incorrect input")
-        exit(-1)
+        raise ValueError("Incorrect input: radius cannot be negative")
     return math.pi * r * r
 
 
 def perimeter(r):
     if r < 0:
-        print("incorrect input")
-        exit(-1)
+        raise ValueError("Incorrect input: radius cannot be negative")
     return 2 * math.pi * r
