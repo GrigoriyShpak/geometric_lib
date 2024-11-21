b690c2301fcc14d4ab12e41935d868d6ac8fa621
Григорий <468117@niuitmo.ru>
2024-11-21 21:08:15 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -2,8 +2,14 @@ import math
 
 
 def area(r):
+    if r < 0:
+        print("incorrect input")
+        exit(-1)
     return math.pi * r * r
 
 
 def perimeter(r):
+    if r < 0:
+        print("incorrect input")
+        exit(-1)
     return 2 * math.pi * r
