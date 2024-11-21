299a38eedeba275eab485001b2a4962c7ef0d16f
Григорий <468117@niuitmo.ru>
2024-11-21 21:09:15 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,6 +1,12 @@
 def area(a):
+    if r < 0:
+        print("incorrect input")
+        exit(-1)
     return a * a
 
 
 def perimeter(a):
+    if r < 0:
+        print("incorrect input")
+        exit(-1)
     return 4 * a
