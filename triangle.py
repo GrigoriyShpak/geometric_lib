cbfe0091bc87b2be38c6cf496f46cf56c6e8f599
Григорий <468117@niuitmo.ru>
2024-11-22 18:33:04 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,11 +1,21 @@
 def area(a, b, c):
-    p = (a + b + c) / 2
     if a < 0 or b < 0 or c < 0:
         raise ValueError("Incorrect input: side lengths cannot be negative")
+    if a + b <= c or a + c <= b or b + c <= a:
+        raise ValueError(
+            "Invalid triangle: the sum of any two sides must be greater than the third"
+        )
+
+    p = (a + b + c) / 2
     return (p * (p - a) * (p - b) * (p - c)) ** 0.5
 
 
 def perimeter(a, b, c):
     if a < 0 or b < 0 or c < 0:
         raise ValueError("Incorrect input: side lengths cannot be negative")
+    if a + b <= c or a + c <= b or b + c <= a:
+        raise ValueError(
+            "Invalid triangle: the sum of any two sides must be greater than the third"
+        )
+
     return a + b + c
