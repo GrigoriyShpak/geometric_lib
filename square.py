018692dbd84fffe52c4d32216e22e83a6f18c368
Григорий <468117@niuitmo.ru>
2024-11-22 17:01:57 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,10 +1,10 @@
 def area(a):
     if a < 0:
-        raise ValueError("Incorrect input: side length cannot be negative")
+        raise AssertionError("Incorrect input: side length cannot be negative")
     return a * a
 
 
 def perimeter(a):
     if a < 0:
-        raise ValueError("Incorrect input: side length cannot be negative")
+        raise AssertionError("Incorrect input: side length cannot be negative")
     return 4 * a
