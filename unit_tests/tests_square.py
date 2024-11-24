f0d53182b20f36dab88c693145f54bf018638931
Григорий <468117@niuitmo.ru>
2024-11-24 12:50:20 +0300

    Update tests_square.py

--- unit_tests/tests_square.py
+++ unit_tests/tests_square.py
@@ -9,29 +9,32 @@ from square import area, perimeter
 class SquareTestCase(unittest.TestCase):
 
     def test_negative_side(self):
-
         side = -1
-
         with self.assertRaises(ValueError):
             area(side)
-
+    def test_negative_side(self):
+        side = -1
         with self.assertRaises(ValueError):
             perimeter(side)
 
     def test_zero_side(self):
-
         side = 0
         excepted_area = 0
         excepted_perimeter = 0
-
         self.assertEqual(area(side), excepted_area)
+    def test_zero_side(self):
+        side = 0
+        excepted_area = 0
+        excepted_perimeter = 0
         self.assertEqual(perimeter(side), excepted_perimeter)
 
     def test_positive_side(self):
-
         side = 1
         excepted_area = 1
         excepted_perimeter = 4
-
         self.assertEqual(area(side), excepted_area)
+    def test_positive_side(self):
+        side = 1
+        excepted_area = 1
+        excepted_perimeter = 4
         self.assertEqual(perimeter(side), excepted_perimeter)
