96c8334bb4d86b4a85ac7815aad3172a0ccd81b1
Григорий <468117@niuitmo.ru>
2024-11-24 13:14:46 +0300

    Update tests_circle.py

--- unit_tests/tests_circle.py
+++ unit_tests/tests_circle.py
@@ -10,41 +10,35 @@ from circle import area, perimeter
 class CircleTestCase(unittest.TestCase):
 
     def test_negative_radius_area(self):
-
         radius = -1
-
         with self.assertRaises(ValueError):
             area(radius)
-            
-     def test_negative_radius_area(self):
 
+    def test_negative_radius_perimeter(self):
         radius = -1
-
         with self.assertRaises(ValueError):
             perimeter(radius)
 
-    def test_zero_radius(self):
-
+    def test_zero_radius_area(self):
         radius = 0
-        excepted_area = 0
+        expected_area = 0
+        self.assertEqual(area(radius), expected_area)
 
-        self.assertEqual(area(radius), excepted_area)
-    def test_zero_radius(self):
-
+    def test_zero_radius_perimeter(self):
         radius = 0
-        excepted_perimeter = 0
+        expected_perimeter = 0
+        self.assertEqual(perimeter(radius), expected_perimeter)
 
-        self.assertEqual(perimeter(radius), excepted_perimeter)
-
-    def test_positive_radius(self):
-
+    def test_positive_radius_area(self):
         radius = 1
-        excepted_area = math.pi
+        expected_area = math.pi
+        self.assertEqual(area(radius), expected_area)
 
-        self.assertEqual(area(radius), excepted_area)
-    def test_positive_radius(self):
-
+    def test_positive_radius_perimeter(self):
         radius = 1
-        excepted_perimeter = 2 * math.pi
+        expected_perimeter = 2 * math.pi
+        self.assertEqual(perimeter(radius), expected_perimeter)
 
-        self.assertEqual(perimeter(radius), excepted_perimeter)
+
+if __name__ == "__main__":
+    unittest.main()
