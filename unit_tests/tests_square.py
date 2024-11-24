1aff18829c7c65a3a5184887dc7c07830094c253
Григорий <468117@niuitmo.ru>
2024-11-24 17:39:01 +0300

    Update tests_square.py

--- unit_tests/tests_square.py
+++ unit_tests/tests_square.py
@@ -20,23 +20,19 @@ class SquareTestCase(unittest.TestCase):
     def test_zero_side(self):
         side = 0
         excepted_area = 0
-        excepted_perimeter = 0
         self.assertEqual(area(side), excepted_area)
 
     def test_zero_side(self):
         side = 0
-        excepted_area = 0
         excepted_perimeter = 0
         self.assertEqual(perimeter(side), excepted_perimeter)
 
     def test_positive_side(self):
         side = 1
         excepted_area = 1
-        excepted_perimeter = 4
         self.assertEqual(area(side), excepted_area)
 
     def test_positive_side(self):
         side = 1
-        excepted_area = 1
         excepted_perimeter = 4
         self.assertEqual(perimeter(side), excepted_perimeter)
